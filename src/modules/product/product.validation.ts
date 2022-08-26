be9698b749694392ff54395cb910d53e1a137f8f
Dai Ho <h.sydai@gmail.com>
2022-08-27 02:27:26 +0700

    feat: api product create

--- src/modules/product/product.validation.ts
+++ src/modules/product/product.validation.ts
@@ -5,147 +5,61 @@ export const ProductValidation = {
   create: validate({
     [Segments.BODY]: Joi.object({
       name: Joi.string(),
-
-      director: Joi.string(),
-
-      contract_addresses: Joi.object(),
-
+      contract_addresses: Joi.array().items(Joi.object()),
       crypto_currencies: Joi.array().items(Joi.string().regex(ObjectIdPattern)),
-
-      //array id of categories
       categories: Joi.array().items(Joi.string().regex(ObjectIdPattern)),
-
       features: Joi.array().items(Joi.string()),
-
-      token: Joi.string(),
-
       apps: Joi.array().items(Joi.object()),
-
       supports: Joi.array().items(Joi.object()),
-
       galleries: Joi.array().items(Joi.string()),
-
       informations: Joi.array().items(Joi.object()),
-
       team: Joi.array().items(Joi.object()),
-
       parent_company: Joi.string(),
-
       team_location: Joi.string(),
-
-      location: Joi.string(),
-
-      research_papers: Joi.array().items(Joi.object()),
-
       sponsored: Joi.boolean(),
-
-      short_description: Joi.string(),
-
+      verified: Joi.boolean(),
       twitter: Joi.string(),
-
       telegram: Joi.string(),
-
       facebook: Joi.string(),
-
-      instagram: Joi.string(),
-
-      linkedin: Joi.string(),
-
-      github: Joi.string(),
-
       medium: Joi.string(),
-
+      discord: Joi.string(),
       youtube: Joi.string(),
-
       website: Joi.string(),
-
       blog: Joi.string(),
-
-      email: Joi.string(),
-
-      tel: Joi.string(),
-
+      reddit: Joi.string(),
       about: Joi.string(),
-
       avatar: Joi.string(),
-
       rocket_chat: Joi.string(),
-
-      bitcoin_talk: Joi.string(),
     }),
   }),
   update: validate({
     [Segments.BODY]: Joi.object({
       name: Joi.string(),
-
-      director: Joi.string(),
-
-      contract_addresses: Joi.object(),
-
+      contract_addresses: Joi.array().items(Joi.object()),
       crypto_currencies: Joi.array().items(Joi.string().regex(ObjectIdPattern)),
-
-      //array id of categories
       categories: Joi.array().items(Joi.string().regex(ObjectIdPattern)),
-
-      software_license: Joi.string(),
-
       features: Joi.array().items(Joi.string()),
-
-      token: Joi.string(),
-
       apps: Joi.array().items(Joi.object()),
-
       supports: Joi.array().items(Joi.object()),
-
       galleries: Joi.array().items(Joi.string()),
-
       informations: Joi.array().items(Joi.object()),
-
       team: Joi.array().items(Joi.object()),
-
       parent_company: Joi.string(),
-
       team_location: Joi.string(),
-
-      location: Joi.string(),
-
-      research_papers: Joi.array().items(Joi.object()),
-
       sponsored: Joi.boolean(),
-
-      short_description: Joi.string(),
-
+      verified: Joi.boolean(),
       twitter: Joi.string(),
-
       telegram: Joi.string(),
-
       facebook: Joi.string(),
-
-      instagram: Joi.string(),
-
-      linkedin: Joi.string(),
-
-      github: Joi.string(),
-
       medium: Joi.string(),
-
+      discord: Joi.string(),
       youtube: Joi.string(),
-
       website: Joi.string(),
-
       blog: Joi.string(),
-
-      email: Joi.string(),
-
-      tel: Joi.string(),
-
+      reddit: Joi.string(),
       about: Joi.string(),
-
       avatar: Joi.string(),
-
       rocket_chat: Joi.string(),
-
-      bitcoin_talk: Joi.string(),
     }),
     [Segments.PARAMS]: Joi.object({
       id: Joi.string().regex(ObjectIdPattern).required(),

a58175c9214197b5759ad854cb898e166e413255
dajneem23 <dajneem23@gmail.com>
2023-01-11 10:02:23 +0700

    feat:  change insert balance_list debank

--- src/modules/debank/debank.service.ts
+++ src/modules/debank/debank.service.ts
@@ -807,7 +807,7 @@ export class DebankService {
           amount,
           updated_at
         )
-        VALUES ($1, $2, $3)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
         `,
           [
             user_address,

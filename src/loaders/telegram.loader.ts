12058cdf118ea0ea6f7b140b2d699fa3f12f3f75
dajneem23 <dajneem23@gmail.com>
2023-04-04 11:24:58 +0700

    fix: env

--- src/loaders/telegram.loader.ts
+++ src/loaders/telegram.loader.ts
@@ -3,9 +3,9 @@ import { env } from 'process';
 import Container, { Token } from 'typedi';
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = env.TELEGRAM_BOT_TOKEN;
 export const telegramBotToken = new Token<TelegramBot>('_telegramBot');
 export const TelegramLoader = async () => {
+  const token = env.TELEGRAM_BOT_TOKEN;
   // Create a bot that uses 'polling' to fetch new updates
   const bot = new TelegramBot(token, { polling: false });
   Container.set(telegramBotToken, bot);

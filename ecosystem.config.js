0e9885b7d4d042e5e6ad6995a8fe39ba00d56830
dajneem23 <dajneem23@gmail.com>
2023-04-04 09:43:29 +0700

    feat: update pm2 cluster

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,8 @@ module.exports = {
       name: 'cronjob-debank',
       script: 'build/server.js',
       exec_mode: 'cluster',
-      instances: '1',
-      max_memory_restart: '1G',
+      instances: '2',
+      max_memory_restart: '2G',
       env: {
         NODE_OPTIONS: '--max-old-space-size=4096',
         NODE_ENV: 'production',

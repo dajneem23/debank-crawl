3b070d1210f906dd023ae9dc0d88e4a8327b48e4
dajneem23 <dajneem23@gmail.com>
2023-04-06 18:25:38 +0700

    feat: revert

--- src/modules/debank/debank.fnc.ts
+++ src/modules/debank/debank.fnc.ts
@@ -954,9 +954,9 @@ export const pageDebankFetchProfileAPI = async ({
   } catch (error) {
     if (retry > 0) {
       await sleep(10 * 1000);
-      // await page.goto(`https://debank.com/profile/${user_address}`, {
-      //   timeout: 1000 * 60,
-      // });
+      await page.goto(`https://debank.com/profile/${user_address}`, {
+        timeout: 1000 * 60,
+      });
       // await page.reload();
       return await pageDebankFetchProfileAPI({ url, retry: retry - 1, page, user_address, timeout });
     } else {
